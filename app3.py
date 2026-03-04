9307a812526b13babb2326fbf48f32701aaed759
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-03-04 17:03:03 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -2005,39 +2005,39 @@ with tabs[0]:
                 ))
 
 # Настройки отображения графика
-        fig.update_layout(
-            barmode='stack',
-            height=750,
-            # Увеличиваем отступ слева (l=150), чтобы влезли длинные названия категорий
-            # Увеличиваем отступ сверху (t=30) для комфортного визуального восприятия
-            margin=dict(l=150, r=20, t=30, b=20),
-            
-            # Ось X скрыта, но если захотите включить шрифт, добавьте tickfont сюда
-            xaxis=dict(visible=False, range=[0, 100]),
-            
-            # Настройка оси Y (Названия категорий)
-            yaxis=dict(
-                autorange="reversed", 
-                # УВЕЛИЧИВАЕМ ШРИФТ НАЗВАНИЙ СЛЕВА
-                tickfont=dict(size=16, family="Arial Black", color="white")
-            ),
-            
-            showlegend=False,
-            plot_bgcolor='rgba(0,0,0,0)',
-            paper_bgcolor='rgba(0,0,0,0)'
-        )
+            fig.update_layout(
+                barmode='stack',
+                height=750,
+                # Увеличиваем отступ слева (l=150), чтобы влезли длинные названия категорий
+                # Увеличиваем отступ сверху (t=30) для комфортного визуального восприятия
+                margin=dict(l=150, r=20, t=30, b=20),
+                
+                # Ось X скрыта, но если захотите включить шрифт, добавьте tickfont сюда
+                xaxis=dict(visible=False, range=[0, 100]),
+                
+                # Настройка оси Y (Названия категорий)
+                yaxis=dict(
+                    autorange="reversed", 
+                    # УВЕЛИЧИВАЕМ ШРИФТ НАЗВАНИЙ СЛЕВА
+                    tickfont=dict(size=16, family="Arial Black", color="white")
+                ),
+                
+                showlegend=False,
+                plot_bgcolor='rgba(0,0,0,0)',
+                paper_bgcolor='rgba(0,0,0,0)'
+            )
 
-        # Если у вас есть цифры внутри столбцов (traces), 
-        # добавьте этот блок для увеличения их шрифта:
-        fig.update_traces(
-            textfont=dict(size=16, family="Arial Black")
-        )
-        
-        
-        # Добавление вертикальных линий сетки для красоты
-        fig.update_xaxes(showgrid=True, gridcolor='lightgrey', dtick=10)
+            # Если у вас есть цифры внутри столбцов (traces), 
+            # добавьте этот блок для увеличения их шрифта:
+            fig.update_traces(
+                textfont=dict(size=16, family="Arial Black")
+            )
+            
+            
+            # Добавление вертикальных линий сетки для красоты
+            fig.update_xaxes(showgrid=True, gridcolor='lightgrey', dtick=10)
 
-        st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True)
 
     # 4. Наполнение правой колонки легендой
     with col_legend:
