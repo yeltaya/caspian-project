4026c2aeb5b963dfbaa80e3be7055fa2546f6ccc
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-03-11 09:29:05 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -3292,7 +3292,7 @@ with tabs[2]:
         st.success("**AgroData** — актуальная информация всегда под рукой.")
         st.info("Прогнозы охватывают все ключевые зерносеющие регионы Казахстана.")
             
-            st.markdown("""
+        st.markdown("""
                     <div style="background: #f1f8e9; padding: 20px; border-radius: 15px; border: 1px dashed #2e7d32; text-align: center;">
                         <h5 style="color: #1b5e20; margin-bottom: 15px;">📲 Приложение AgroData</h5>
                         <img src="https://img.icons8.com/ios/100/2e7d32/qr-code--v1.png" width="100">
@@ -3301,7 +3301,7 @@ with tabs[2]:
                             <button style="background: #2e7d32; color: white; border: none; padding: 8px 15px; border-radius: 5px; cursor: pointer;">Открыть портал</button>
                         </a>
                     </div>
-                """, unsafe_allow_html=True)
+        """, unsafe_allow_html=True)
                 
 
     with col2:
