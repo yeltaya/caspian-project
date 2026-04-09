9224323d8c94024bb70a5d74a8be10cc7eebee32
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-04-09 11:09:05 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -2651,7 +2651,7 @@ with tabs[1]:
         img_path = os.path.join(BASE_DIR, img_filename)
         
         # Создаем две колонки: для карты и для пояснительного текста
-        col_map, col_text = st.columns([2, 1])
+        col_map, col_text = st.columns([3, 1])
         
         with col_map:
             # Проверка наличия файла
