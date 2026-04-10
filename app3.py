623ea337232af845c81eb7346ee86d6ccdf3908d
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-04-10 08:15:59 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -4801,12 +4801,12 @@ with tabs[4]:
                         f"""
                         <div style="width: 100%; display: flex; flex-direction: column; align-items: center;">
                             <img src="data:image/png;base64,{data}" 
-                                 style="width: 70%; 
+                                 style="width: 100%; 
                                         max-width: none; 
                                         margin-left: 0%; 
                                         border-radius: 8px; 
                                         box-shadow: 0 4px 12px rgba(0,0,0,0.15);">
-                            <p style="color: gray; font-size: 0.85rem; text-align: center; margin-top: 10px; width: 70%; margin-left: -17.5%;">
+                            <p style="color: gray; font-size: 0.85rem; text-align: center; margin-top: 10px; width: 100%; margin-left: -17.5%;">
                                 Входные ГИС-данные (почвы FAO, Land 30, гидротопы)
                             </p>
                         </div>
@@ -4864,7 +4864,7 @@ with tabs[4]:
                 st.markdown(
                     f"""
                     <div style="width: 100%;">
-                        <img src="data:image/jpeg;base64,{data}" style="width: 0%; border-radius: 10px; box-shadow: 0 4px 10px rgba(0,0,0,0.1);">
+                        <img src="data:image/jpeg;base64,{data}" style="width: 100%; border-radius: 10px; box-shadow: 0 4px 10px rgba(0,0,0,0.1);">
                         <p style="color: gray; font-size: 0.85rem; text-align: center; margin-top: 10px;">Карта селевой опасности территории РК</p>
                     </div>
                     """, unsafe_allow_html=True
