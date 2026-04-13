3c7f9ee2113a0b9aa0aef02a00f76e6d4144d493
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-04-13 15:52:01 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -3448,10 +3448,10 @@ with tabs[1]:
 
 
     if 'lang' not in st.session_state:
-        st.session_state.lang = 'RU'
+        st.session_state.lang = 'ru'
 
     # Выбор языка
-    lang_choice = st.selectbox("Language", ["RU", "KZ", "EN"], key="language_selector")
+    lang_choice = st.selectbox("Language", ["ru", "kz", "en"], key="language_selector")
     st.session_state.lang = lang_choice
                     
     import streamlit as st
