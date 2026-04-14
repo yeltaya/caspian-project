0e676017e54a004e9e0d5696eccc0e8ed2ef69a5
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-04-14 15:25:25 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -6500,12 +6500,6 @@ with tabs[5]:
     # Используйте t['local'], t['inflow'] в параметрах name= для go.Bar
     
     
-                
-      
-        import streamlit as st
-        import pandas as pd
-        import plotly.graph_objects as go
-        import numpy as np
 
         # 1. Подготовка данных
         data = {
