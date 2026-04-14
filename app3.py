81c5b5e19a0bc341f8453d509c0d1d3638692b80
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-04-14 13:48:12 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -6297,10 +6297,6 @@ with tabs[4]:
 
         st.divider()
 
-    # Вызов функции
-    render_selevidenie_section(st)
-
-
 
 # --- 1. СИНХРОНИЗАЦИЯ ЯЗЫКА ---
     if 'lang_code' in locals() or 'lang_code' in globals():
