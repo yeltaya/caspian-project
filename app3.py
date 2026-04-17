47c199031708fe121049ef5d8ed93af1a59b64c4
yeltaya <98028569+yeltaya@users.noreply.github.com>
2026-04-17 10:06:40 +0500

    Add files via upload

--- app3.py
+++ app3.py
@@ -7391,16 +7391,15 @@ with tabs[5]:
         with st.container(border=is_active):
             st.markdown(f"### {'🌟' if is_active else '🔹'} {name}")
             
-            img_col, info_col = st.columns([3.5, 1])
+            img_col, info_col = st.columns([1, 1])
             
             with img_col:
                 if os.path.exists(photo_path):
-                    # use_container_width=True растянет фото на все 3.5 доли колонки
-                    st.image(photo_path, use_container_width=True)
-                    st.caption(f"🗺️ {name}") # Маленькая подпись под картой
+                    st.image(photo_path, width=800, caption=f"Вид бассейна: {name}")
                 else:
                     st.info(f"📸 Фото для {name} ожидается")
-        
+                    st.image("https://via.placeholder.com/600x400?text=Photo+Missing", use_container_width=True)
+            
             with info_col:
                 st.markdown(f"##### 📝 Гидрологическая справка: {name}")
                 
